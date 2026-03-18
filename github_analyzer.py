1b0ffbd7d7f39d8fd6d10f376e92beee680dd144
Andrei Gurdumov <andreigurd@gmail.com>
2026-03-18 18:28:30 -0400

    Added user profile information display.

--- github_analyzer.py
+++ github_analyzer.py
@@ -1,6 +1,6 @@
 import requests
-import os
 from datetime import datetime
+from collections import Counter
 
 class GitHub_Analyzer:
 
@@ -17,12 +17,13 @@ class GitHub_Analyzer:
             if response.status_code == 200:
                 print("User Status Code:", response.status_code)
             # Get specific data from the JSON
-            
-                user_data = response.json()       
-                print("Name:", user_data.get('name') or "N/A")
-                print("Bio:", user_data.get('bio') or "N/A")
-                print("Location:", user_data.get('location') or "N/A")
-                print("Public Repos:", user_data.get('public_repos', "N/A"))
+
+                # use self. store data in class so other methods can use it.
+                self.user_data = response.json()       
+                # print("Name:", self.user_data.get('name') or "N/A")
+                # print("Bio:", self.user_data.get('bio') or "N/A")
+                # print("Location:", self.user_data.get('location') or "N/A")
+                # print("Public Repos:", self.user_data.get('public_repos', "N/A"))
                 # .get(key, default) wont replace 0 with N/A
                     
                     
@@ -50,7 +51,7 @@ class GitHub_Analyzer:
             if response.status_code == 200:
                 print("User_Repositories Status Code:", response.status_code)
             # Get specific data from the JSON
-                user_repo_data = response.json()     
+                self.repo_data = response.json()     
                 
             elif response.status_code == 404:
                 print(f"Repositories for '{self.user_name}' not found")
@@ -72,8 +73,8 @@ class GitHub_Analyzer:
         
         #------------ Total stars across all repos
 
-        stars_count = {}
-        for star_item in self.user_repo_data[:5]:            
+        stars_count = 0
+        for star_item in self.repo_data[:5]:            
             if stars_count:
                 stars_count += star_item.get('stargazers_count')
             else:
@@ -81,7 +82,7 @@ class GitHub_Analyzer:
 
         #------------ Most popular repository
         most_stars = []
-        for star_item in self.user_repo_data[:5]:
+        for star_item in self.repo_data[:5]:
             if not most_stars:
                 most_stars = star_item
             elif star_item.get('stargazers_count') > most_stars.get('stargazers_count'):
@@ -90,21 +91,31 @@ class GitHub_Analyzer:
         #------------ Primary language (most repos in that language)
 
         languages_used = []
-        for lang_item in self.user_repo_data[:5]:            
+        for lang_item in self.repo_data[:5]:            
             if lang_item['language']:
                 languages_used.append(lang_item['language']) 
 
-        # Counter and .most(n) returns a list of tuples of element and count in descending order. (n) argument for how many tuples are returned.
-        primary_language = Counter(languages_used).most(1)           
+        # Counter and .most_common(n) returns a list of tuples of element and count in descending order. (n) argument for how many tuples are returned.
+        primary_language = Counter(languages_used).most_common(1)           
 
         #------------ Total forks across all repos
 
-        forks_count = {}
-        for fork_item in self.user_repo_data[:5]:            
+        forks_count = 0
+        for fork_item in self.repo_data[:5]:            
             if forks_count:
                 forks_count += fork_item.get('forks_count')
             else:
                 forks_count = fork_item.get('forks_count')
+
+        # return 3+ variables to be used elsewhere. note to turn to dictionary for easy access.
+        # note returning loose variables are order sensitive for accessing later.
+        # the values will belong to dictionary "stats"
+        return {
+            "total_stars" :stars_count,
+            "total_forks": forks_count, 
+            "most_stars": most_stars,
+            "primary_language": primary_language
+        }
 
     #----------------------------------------------------------
     def display_stats(self):
@@ -116,14 +127,21 @@ class GitHub_Analyzer:
         print(f"GitHub Profile Dashboard for: {self.user_name}")
         print(f"{'='*60}\n")
 
-        print(f"Total Stars: {stats.stars_count}\n")
-        print(f"Most Popular Repository: {stats.most_stars["name"]}\n")
-        print(f"Primary Language Used: {stats.primary_language[0][0]}\n")
-        # primary_language is counter result of tuples of language and count. want to display language name.
+        print("User Info:")
+        print("Name:", self.user_data.get('name') or "N/A")
+        print("Bio:", self.user_data.get('bio') or "N/A")
+        print("Location:", self.user_data.get('location') or "N/A")
+        print("Public Repos:", self.user_data.get('public_repos', "N/A"))
+
+        print(f"\nTotal Stars: {stats['total_stars']}")
+        print(f"Most Popular Repository: {stats['most_stars']['name']}")
+        print(f"Primary Language Used: {stats['primary_language'][0][0]}")
+        # note that primary_language is counter result of tuples of language and count. want to display language name.
+        print(f"Total Forks Accross All Repositories: {stats['total_forks']}\n")
 
 #----------------------------------------------------------
 def main():
-    dashboard = GitHub_Analyzer()
+    
 
     # ask for user name and run get_user function
     while True:
@@ -133,13 +151,15 @@ def main():
             break
         elif user_name.lower() == '':
             print("Entry cannot be blank. Please try again.")
+            continue
         else:
             break
 
-        dashboard.get_user(user_name)
-        dashboard.get_user_repos(user_name)
-        # run  dashboard.calculate_stats(user_name) inside of display_stats
-        dashboard.display_stats(user_name)
+    dashboard = GitHub_Analyzer(user_name)
+    dashboard.get_user()
+    dashboard.get_user_repos()
+    # run  dashboard.calculate_stats() inside of display_stats
+    dashboard.display_stats()
 
 if __name__ == "__main__":
     main()
